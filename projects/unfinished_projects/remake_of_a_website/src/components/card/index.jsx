85ea34a39d0a1dd7cbbec3728f70daaaf223e85c
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-08-26 14:40:07 -0300

    Made updates to the website

---
+++ projects/unfinished_projects/remake_of_a_website/src/components/card/index.jsx
@@ -0,0 +1,13 @@
+export default function card(){
+	return(
+		<>
+			<div className="wrapper">
+				<h2></h2>
+				<img src="" alt=""/>
+				<p></p>
+				<p>Price: USD<b></b></p>
+			</div>
+
+		</>
+	);
+}
---
+++ projects/unfinished_projects/remake_of_a_website/src/pages/produtos.jsx
@@ -0,0 +1,30 @@
+export default function products(){
+const products = [
+	{
+		name:"Dish soap",
+		desc:"Used to wash dishes and related things",
+		img:"dish_washing.jpeg",
+		price_usd: 2,
+	}
+	{
+		name:"Powdered laundry detergent",
+		desc:"Made for washing your clothes",
+		img:"powdered.jpeg",
+		price_usd: 5,
+	}
+	{
+		name:"Soap",
+		desc:"Common soap, used to wash your body parts",
+		img:"soap.jpeg",
+		price_usd: 1,
+	}
+];
+	return(
+		<>
+			<h1>Page for products listing</h1>
+			
+		{/*this is the place where there will be the cards*/}
+		</>
+
+	)
+}
