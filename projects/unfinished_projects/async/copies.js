37903db0b2e4c751007689d1b87c7579173f6090
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-08-28 17:07:13 -0300

    Feat: added a console.log to update

--- projects/unfinished_projects/async/copies.js
+++ projects/unfinished_projects/async/copies.js
@@ -2,7 +2,13 @@ const original ={
 	id: 1,
 	to_name:'milk',
 	amount:5
-}
+};
 
-const updated = {...original, storage:150}
+const updated = {...original, amount:150};
+
+console.log({original, updated})
+
+const {to_name, amount} = updated;
+
+console.log(`${to_name} has ${amount} unit(s)`);
 
