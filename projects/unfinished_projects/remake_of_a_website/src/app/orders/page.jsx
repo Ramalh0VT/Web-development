d401be874b33f21d328d2aa13e874cb904486bd9
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-09-09 15:22:55 -0300

    Feat: updated the orders page

--- projects/unfinished_projects/remake_of_a_website/src/app/orders/page.jsx
+++ projects/unfinished_projects/remake_of_a_website/src/app/orders/page.jsx
@@ -5,21 +5,46 @@ export default function orders(){
 	const [orders_var, setOrder] = useState([]);
 	
 
-	const [client, SetClient] = useState("");
+	const [client, setClient] = useState("");
 	const [equipment, SetEquipment] = useState("");
-	const [desc, SetDesc] = useState("");
+	const [desc, setDesc] = useState("");
 
 	const [error, setError] = useState("");
 	
+	function submitOrder(event){
+		event.preventDefault();
+
+		if(client.trim() === ""  || equipment.trim() === "" || desc.trim() ===""){
+			setError("Fill in all fields!");
+			return;
+		}
+		setError("");
+	}
+
+	const newOrder = {
+		id: Date.now(),
+		client: client.trim(),
+		equipment:equipment.trim(),
+		desc:desc.trim(),
+		status:"open"
+	}
+	setOrders([...orders_var, newOrder])
+	setClient("")
+	setEquipment("")
+	setDesc("")
+	setError("")
 	return(
 		<main>
 			<h1>Service orders</h1>
 			<form>
 				<div>
 					<label htmlFor="client"></label>
-			<input type="text" name="client" id="client" value="{client}" placeholder="Client's name">	
+			<input type="text" name="client" id="client" value="{client}" placeholder="Client's name" onChange={(event) => setClient(event.target.value)}>	
+			<input type="text" name="equipment" id="equipment" value="{equipment}" placeholder="E.g. laptop" onChange={(event) => setEquipment(event.target.value)}>	
+			<input type="text" name="desc" id="desc" value="{desc}" placeholder="Description" onChange={(event) => setDesc(event.target.value)}>	
 			</div>		
 			</form>
+			<h1>{error}</h1>
 		</main>
 	)
 }
