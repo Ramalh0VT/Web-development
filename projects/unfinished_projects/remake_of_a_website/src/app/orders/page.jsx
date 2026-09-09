3973139ffea0e97da45eedab5f43b9b483fbe8dc
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-09-09 15:03:52 -0300

    Feat: added a prototype version of a forms with js

--- projects/unfinished_projects/remake_of_a_website/src/app/orders/page.jsx
+++ projects/unfinished_projects/remake_of_a_website/src/app/orders/page.jsx
@@ -9,4 +9,17 @@ export default function orders(){
 	const [equipment, SetEquipment] = useState("");
 	const [desc, SetDesc] = useState("");
 
+	const [error, setError] = useState("");
+	
+	return(
+		<main>
+			<h1>Service orders</h1>
+			<form>
+				<div>
+					<label htmlFor="client"></label>
+			<input type="text" name="client" id="client" value="{client}" placeholder="Client's name">	
+			</div>		
+			</form>
+		</main>
+	)
 }
