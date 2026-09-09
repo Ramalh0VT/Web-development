e3a97c6d55f8843e52e03435fb24ffd459a79817
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-09-09 14:22:05 -0300

    Feat: added a page in the application to count

---
+++ projects/unfinished_projects/remake_of_a_website/src/app/counter/page.jsx
@@ -0,0 +1,12 @@
+export default function counter(){
+	return(
+		<>
+			<h1>A counter</h1>
+			<p>Value: <i>{count_val}</i></p>
+			<div>
+				<button type="button"> + </button>				
+				<button type="button"> + </button>
+			</div>
+		</>
+	)
+}
