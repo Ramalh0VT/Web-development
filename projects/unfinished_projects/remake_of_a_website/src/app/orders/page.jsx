4c1c38581bdbb87c6c14ddf1d3c64382f1abf0b8
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-09-09 14:59:04 -0300

    Feat: added a order page

---
+++ projects/unfinished_projects/remake_of_a_website/src/app/orders/page.jsx
@@ -0,0 +1,12 @@
+"use client"
+import { useState } from "react"
+
+export default function orders(){
+	const [orders_var, setOrder] = useState([]);
+	
+
+	const [client, SetClient] = useState("");
+	const [equipment, SetEquipment] = useState("");
+	const [desc, SetDesc] = useState("");
+
+}
