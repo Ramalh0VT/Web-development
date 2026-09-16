1f8b6bbdb04cbf083c5d565331d04912d87bcc6e
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-09-16 15:37:39 -0300

    Feat: started a brand new project

---
+++ projects/unfinished_projects/recipes/src/app/components/index.jsx
@@ -0,0 +1,10 @@
+export default function recipe_returner(){
+	const [recipesArray, setRecipes] = useState([]);
+	const [errorMsg, setErrorMsg] = useState
+	
+	return(
+		<>
+			
+		</>
+	);
+}
---
+++ projects/unfinished_projects/recipes/src/app/recipes/page.jsx
@@ -0,0 +1,10 @@
+import "../globals.css"
+
+export default function recipes_page(){
+	return(
+		<>
+			<h1>List of recipes: </h1>
+
+		</>
+	)
+}
