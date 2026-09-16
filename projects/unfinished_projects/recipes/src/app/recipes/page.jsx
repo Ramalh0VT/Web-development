329bbae11a09ff917842e531f0cd26c861e9bcf6
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-09-16 16:47:42 -0300

    Feat: trying to fix a error

--- projects/unfinished_projects/recipes/src/app/recipes/page.jsx
+++ projects/unfinished_projects/recipes/src/app/recipes/page.jsx
@@ -1,10 +1,37 @@
-import "../globals.css"
-import recipes_giver from "@/app/components/recipes_giver" 
-export default function recipes_page(){
+"use client";
+import "../globals.css";
+import { useState, useEffect } from 'react';
+import "../components/recipes_giver/"
+
+export default function recipe_returner(){
+	const [recipesArray, setRecipes] = useState([]);
+	const [errorMsg, setErrorMsg] = useState("");	
+	useEffect( () => {
+		fetch("https://dummyjson.com/recipes/")
+		.then(res => res.json())
+		.then(info => {
+			console.log(info);
+			setRecipes(info.recipes);
+			setErrorMsg("");
+		})
+		.catch(error => setErrorMsg(error.message))
+	}, [] );
 	return(
-		<>
-			<h1>List of recipes: </h1>
-			<recipes_giver/>
-		</>
-	)
+		<div>
+			{errorMsg != "" && <p>An error occured: {errorMsg}</p>}
+			{recipesArray.length > 0 ?
+					<div>
+						{recipesArray.map((recipe, idx) => {
+							return(
+								<recipe_card(recipe)/>
+							);
+						})}
+					</div>
+					:
+					<div>
+						<p>No recipes at the moment :(</p>
+					</div>
+			}
+		</div>
+	);
 }
