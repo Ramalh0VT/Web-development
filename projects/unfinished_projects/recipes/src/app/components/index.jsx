60d4c39c6ed8c65c71a2aa7637065afeb87c0d5f
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-09-16 16:08:52 -0300

    Feat: updated the new project

--- projects/unfinished_projects/recipes/src/app/components/index.jsx
+++ projects/unfinished_projects/recipes/src/app/components/index.jsx
@@ -1,5 +1,9 @@
 "use client";
 
+import "../globals.css";
+
+import { useState, useEffect } from 'react';
+
 export default function recipe_returner(){
 	const [recipesArray, setRecipes] = useState([]);
 	const [errorMsg, setErrorMsg] = useState("");
@@ -16,8 +20,23 @@ export default function recipe_returner(){
 
 	}, []);
 	return(
-		<>
-			
-		</>
+		<div>
+			{errorMsg != "" && <p>An error occured: {errorMsg}</p>}
+			{recipesArray.length > 0 ?
+					<div>
+						{recipesArray.map((user, idx) => {
+							return(
+								<div>
+									// recipes info here
+								</div>
+							);
+						})}
+					</div>
+					:
+					<div>
+						<p>No recipes at the moment :(</p>
+					</div>
+			}
+		</div>
 	);
 }
