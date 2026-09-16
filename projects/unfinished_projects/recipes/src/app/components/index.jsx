ae9d30a51aba1942252fa9608dd0d913caee95c0
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-09-16 15:45:21 -0300

    Feat: updated the system

--- projects/unfinished_projects/recipes/src/app/components/index.jsx
+++ projects/unfinished_projects/recipes/src/app/components/index.jsx
@@ -1,7 +1,20 @@
+"use client";
+
 export default function recipe_returner(){
 	const [recipesArray, setRecipes] = useState([]);
-	const [errorMsg, setErrorMsg] = useState
+	const [errorMsg, setErrorMsg] = useState("");
 	
+	useEffect( () => {
+		fetch('https://dummyjson.com/recipes/10')
+		.then(res => res.json())
+		.then(info =>{
+			console.log(info);
+			setRecipes(info.recipes);
+			setError("");
+		});
+		.catch(error => setErrorMsg(error.message))
+
+	}, []);
 	return(
 		<>
 			
