0953e08b6e8aa27ea8760559730afb73bcd2f23b
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-09-16 16:18:15 -0300

    Feat: added a prototype version of the app

--- projects/unfinished_projects/recipes/src/app/recipes/page.jsx
+++ projects/unfinished_projects/recipes/src/app/recipes/page.jsx
@@ -1,10 +1,10 @@
 import "../globals.css"
-
+import recipes_giver from "@/app/components/recipes_giver" 
 export default function recipes_page(){
 	return(
 		<>
 			<h1>List of recipes: </h1>
-
+			<recipes_giver/>
 		</>
 	)
 }
