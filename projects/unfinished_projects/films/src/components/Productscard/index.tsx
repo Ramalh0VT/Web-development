422fc15658af8d4746acbc8a512b14a948c3648d
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-09-23 16:54:37 -0300

    Feat: updated the component

---
+++ projects/unfinished_projects/films/src/components/Productscard/index.tsx
@@ -0,0 +1,12 @@
+// import "./Productscard.css"
+
+export default function Productscard({product}){
+	
+	return(
+		<div className="product-wrapper">
+			<h1>{product.title}</h1>
+			<img src={} alt= "Product image"/>
+
+		</div>
+	)
+}
