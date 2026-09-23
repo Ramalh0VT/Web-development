42fc0396aa0bafef04488182c18f8210c1fa7ec6
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-09-23 15:33:29 -0300

    mass update

---
+++ projects/unfinished_projects/films/src/app/films/[id]/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+import "./filmId.css"
+
+import { useParams } from "next/navigation";
+
+import { useState, useEffect } from "react";
+import data from "@/films.json"
+
+export default function Film(){
+	const [film, setFilm] = useState({});
+	const params = useParams();
+	useEffect( () => {
+		const foundFilm = data.find(f => f.id == params.id);
+		setFilm(foundFilm);
+	}, [])
+
+	return(
+		<>
+			{film != null &&
+			<>
+				<h1>Film title: {film.titulo}</h1>
+				<h2>Release date: {film.ano}</h2>
+				<p><b>DESCRIPTION</b>: {film.sinopse}</p>
+				<img src={film.imagem} width="250" alt="film image"/>
+				<a href={film.trailer} >Film link</a>
+			</>
+			}
+		</>
+	)
+}
