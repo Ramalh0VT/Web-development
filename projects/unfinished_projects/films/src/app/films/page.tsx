d26612209d8244ffd5da97425a2a81155d58b946
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-09-23 14:42:22 -0300

    Feat: updated the films page

--- projects/unfinished_projects/films/src/app/films/page.tsx
+++ projects/unfinished_projects/films/src/app/films/page.tsx
@@ -1,12 +1,19 @@
 "use client";
-
+import data from "@/films.json";
 import { useState, useEffect } from "react";
 
 export default function Films(){
 	const [filmsArray, setFilmsArray] = useState([]);
+	useEffect( () => {
+		setFilmsArray(data);
+	}, [])
 	return(
 		<>
-
+			{filmsArray.length > 0 &&
+				<div>
+						
+				</div>
+			}
 		</>
 	);
 }
