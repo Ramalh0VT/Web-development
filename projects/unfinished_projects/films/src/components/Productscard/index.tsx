48411b1f56694b622a222ea01833ca72942386c4
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-09-23 16:59:08 -0300

    Feat: updated the index

--- projects/unfinished_projects/films/src/components/Productscard/index.tsx
+++ projects/unfinished_projects/films/src/components/Productscard/index.tsx
@@ -1,12 +1,11 @@
 // import "./Productscard.css"
 
 export default function Productscard({product}){
-	
 	return(
 		<div className="product-wrapper">
 			<h1>{product.title}</h1>
 			<img src={} alt= "Product image"/>
-
+			<a href={`/products/${products.id}}`>Know more!<a/>
 		</div>
 	)
 }
